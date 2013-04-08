41dfd368973bef70ddeeca4fa672141c880ed56e
GrNik <gr_nik@mail.ru>
2013-04-08 17:53:36 +0400

    Read excel

--- ExcelToXML/Main.cs
+++ ExcelToXML/Main.cs
@@ -10,8 +10,12 @@ using Excel = Microsoft.Office.Interop.Excel;
 
 namespace ExcelToXML
 {
+    using System.IO;
+
     public partial class Main : Form
     {
+        const string channel = "MIR";
+
         private Excel.Application excelapp;
         private Excel.Window excelWindow;
 
@@ -22,6 +26,7 @@ namespace ExcelToXML
         private Excel.Worksheet excelworksheet;
         private Excel.Range excelcells;
 
+        private StreamWriter file;
 
         public Main()
         {
@@ -32,6 +37,7 @@ namespace ExcelToXML
         {
             excelapp = new Excel.Application();
             excelapp.Visible = true;
+
             try
             {
                 excelapp.Workbooks.Open(txtExcelFile.Text,
@@ -53,15 +59,53 @@ namespace ExcelToXML
                 excelworksheet = (Excel.Worksheet)excelsheets.get_Item(1);
                 //Выделение группы ячеек
 
-                excelcells = excelworksheet.get_Range("A1", Type.Missing);
-                string sStr = Convert.ToString(excelcells.Value2);
+                //excelcells = excelworksheet.get_Range("A1", Type.Missing);
+                //string sStr = Convert.ToString(excelcells.Value2);
 
-                sStr = excelworksheet.Cells[1, 1].Value2.ToString();
+                this.CreateFile(txtXMLFile.Text);
+                int row = 1;
+                try
+                {
+
+                    while (!String.IsNullOrEmpty(excelworksheet.Cells[row, 1].Value2.ToString()))
+                    {
+                        string A = excelworksheet.get_Range("A" + row.ToString(), Type.Missing).Value2.ToString();
+                        string B = excelworksheet.get_Range("B" + row.ToString(), Type.Missing).Value2.ToString();
+                        string C = excelworksheet.get_Range("C" + row.ToString(), Type.Missing).Value2.ToString();
+                        DateTime date = Convert.ToDateTime(A);
+                        DateTime start = date + DoubleToTimeSpan(Convert.ToDouble(B));
+                        DateTime stop = start + DoubleToTimeSpan(Convert.ToDouble(C));
+
+                        string title = excelworksheet.Cells[row, 5].Value2.ToString();
+                        string rating = "";
+                        string desc = excelworksheet.Cells[row, 8].Value2.ToString();
+                        AddProgramm(start, stop, channel, title, date, rating, desc)
+                        ;
+
+                        row++;
+                    }
+                    MessageBox.Show("Была обработано " + (row - 1).ToString() + " строк");
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show(
+                        "При обработке файла возникла ошибка: " + exc.Message + " в строке " + row.ToString(),
+                        "Конвертация файла",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    this.CloseFile();
+                }
             }
-            catch (Exception)
+            catch (Exception exc)
             {
-
-                throw;
+                MessageBox.Show(
+                    "При открытии файла возникла ошибка: " + exc.Message,
+                    "Конвертация файла",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
             }
             finally
             {
@@ -83,6 +127,51 @@ namespace ExcelToXML
             {
                 txtXMLFile.Text = sfdXMLFile.FileName;
             }
+        }
+
+        void CreateFile(string fileName)
+        {
+            file = File.CreateText(fileName);
+            file.WriteLine(@"<?xml version=""1.0"" encoding=""UTF-8"" ?>");
+            file.WriteLine(@"<tv>");
+            file.WriteLine(@"<channel id=""" + channel + @""">");
+            file.WriteLine(@"<display-name lang=""ru"">МИР</display-name>");
+            file.WriteLine(@"</channel>");
+
+        }
+
+        void CloseFile()
+        {
+            file.WriteLine(@"</tv>");
+            file.Flush();
+            file.Close();
+        }
+
+        void AddProgramm(DateTime start, DateTime stop, string channel, string title, DateTime date, string rating, string desc)
+        {
+            StringBuilder programm = new StringBuilder();
+            programm.AppendFormat(@"<programme start=""{0}"" +0400"" stop=""{1}"" +0400"" channel=""{2}"">", start.ToString("yyyyMMddHHmmss"), stop.ToString("yyyyMMddHHmmss"), channel);
+            programm.AppendFormat(@"<title lang=""ru"">{0}</title>", title);
+            programm.AppendFormat(@"<date>{0}</date>", date.ToString("yyyyMMdd"));
+            programm.AppendFormat(@"<rating system=""RU""><value>{0}</value></rating>", rating);
+            programm.AppendFormat(@"<desc lang=""ru"">{0}</desc>", desc);
+            programm.Append(@"</programme>");
+
+            file.WriteLine(programm.ToString());
+        }
+
+        TimeSpan DoubleToTimeSpan(double tm)
+        {
+            tm = tm * 24;
+            int hour = (int)Math.Truncate(tm);
+
+            tm = (tm - hour)*60;
+            int minut = (int)Math.Truncate(tm);
+
+            tm = (tm - minut) * 60;
+            int second = (int)Math.Truncate(tm);
+
+            return new TimeSpan(hour, minut, second);
         }
     }
 }
